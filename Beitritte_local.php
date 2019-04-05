d2cc2b195797f841d02ec5c322ad1aac1563e65e
3-mann <xhoch3.info@gmail.com>
2019-04-05 16:59:40 +0200

    Try with TableFormat

--- Beitritte_local.php
+++ Beitritte_local.php
@@ -8,11 +8,11 @@
 		<h3>...hier holen wir die Beitrittsdaten aus der MySQL Datenbank</h3>
 	<?php 
 		$db_link = mysqli_connect('localhost', 'combit','' ,'Beitritte'); 
-		$db_rc01 = mysqli_query($db_link,"Select * from beitritte")
+		$result = mysqli_query($db_link,"Select * from beitritte")
 			or die("Fehler: ". mysqli_error($db_link));
                 $ausgabe = "<table>";
                 $z = 0;
-		while($row = mysqli_fetch_array($db_rc01 , MYSQL_NUM))
+		while($fetch = mysqli_fetch_array($result , MYSQLI_NUM))
 		{
 			$z ++;
                         if ($z == 1)
@@ -20,14 +20,14 @@
                             $ausgabe .= "<tr>";
                             for ($i=0;$i<=count($fetch)-1;$i++)
                             {
-                                $ausgabe .= "<th>" . mysql_field_name($result,$i) . "</th>";
+                                $ausgabe .= "<th>" . mysqli_fetch_field_direct($result,$i)->name . "</th>";
                             }
                             $ausgabe .= "</tr>";
                         }
                         $ausgabe .= "<tr>";
                         for ($i=0; $i<=count($fetch)-1;$i++)
                         {
-                            $ausgabe .= "<td>" . $fetch($i) . "</td>";
+                            $ausgabe .= "<td>" . $fetch[$i] . "</td>";
                         }
                         $ausgabe .= "<tr>";
                 }
